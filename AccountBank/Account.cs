cb03f7dc0bb5cabfe17ce920c3a93428b26987d4
Данила Тяпкин <dtapkin6@gmail.com>
2023-10-20 11:29:53 +0300

    Добавьте файлы проекта.

---
+++ AccountBank/Account.cs
@@ -0,0 +1,34 @@
+﻿public class BankAccount
+{
+    private double balance;
+    private object @lock = new();
+
+    public double Balance
+    {
+        get { return balance; }
+    }
+    public void TopUpBalance(double summ)
+    {
+        lock (@lock)
+        {
+            balance += summ;
+            Console.WriteLine("Пополнено на: {0} рублей.\nБаланс: {1}", summ, balance);
+            Monitor.Pulse(@lock);
+        }
+    }
+
+    public void WithdrawMoney(double summ)
+    {
+        lock (@lock)
+        {
+            while (balance < summ)
+            {
+                Console.WriteLine("Ожидание пополнения баланса до {0} рублей.", summ);
+                Monitor.Wait(@lock);
+            }
+
+            balance -= summ;
+            Console.WriteLine("Снято: {0} рублей.\nБаланс составляет: {1}", summ, balance);
+        }
+    }
+}
---
+++ AccountBank/Program.cs
@@ -0,0 +1,25 @@
+﻿
+public class Program
+{
+    static void Main()
+    {
+        BankAccount acc = new();
+
+        Thread depositThread = new(() =>
+        {
+            Random r = new();
+            while (true)
+            {
+                double summ = r.Next(100, 1000);
+                acc.TopUpBalance(summ);
+                Thread.Sleep(1000);
+            }
+        });
+
+        depositThread.Start();
+
+        acc.WithdrawMoney(5000);
+
+        Console.WriteLine($"Остаток на балансе: {acc.Balance}");
+    }
+}
